6eafe3428e73f4273151c098d838e10d71284fec
QAQ <115549499+QAQ-0203@users.noreply.github.com>
2024-08-08 21:25:24 +0800

    Upload BaseN_Web.py

---
+++ BaseN_Web.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import Base_N
+
+st.set_page_config(page_title="Simple BaseN-ASCII Converter", page_icon=None, layout="centered", initial_sidebar_state="auto")
+st.title("Simple BaseN-ASCII Converter")
+
+
